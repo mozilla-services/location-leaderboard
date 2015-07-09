490a731987bfc41849065b4d421e4a7a893069d5
Jared Kerim <jared.l.kerim@gmail.com>
2015-07-09 20:24:13 +0000

    Merged contributors migrations

--- leaderboard/contributors/migrations/0001_initial.py
+++ leaderboard/contributors/migrations/0001_initial.py
@@ -7,6 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('locations', '0001_initial'),
     ]
 
     operations = [
@@ -17,5 +18,20 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=255)),
                 ('email', models.EmailField(max_length=254)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Contribution',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('observations', models.PositiveIntegerField()),
+                ('contributor', models.ForeignKey(to='contributors.Contributor')),
+                ('tile', models.ForeignKey(to='locations.Tile')),
+            ],
+        ),
+        migrations.AlterField(
+            model_name='contributor',
+            name='email',
+            field=models.EmailField(unique=True, max_length=254),
         ),
     ]
