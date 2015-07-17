84e3afb03ed0ca52f11b8d69e200c0b3fe831ed0
Jared Kerim <jared.l.kerim@gmail.com>
2015-07-17 16:26:25 +0000

    Revert "Break flake8 compliance"
    This reverts commit d86dbe083050848d93dd5baba6663d13ac76daf2.

--- leaderboard/locations/models.py
+++ leaderboard/locations/models.py
@@ -58,7 +58,7 @@ class TileManager(models.GeoManager):
     def get_or_create_nearest_tile(self, easting=None, northing=None,
                                    *args, **kwargs):
         # Round to the nearest tile size
-        easting = easting - (easting % self.model.TILE_SIZE)#derp
+        easting = easting - (easting % self.model.TILE_SIZE)
         northing = northing - (northing % self.model.TILE_SIZE)
 
         return self.get_or_create(
