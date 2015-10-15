b2b46218c43de7f2604e0b67425614a90a89cad3
Jared Kerim <jared.l.kerim@gmail.com>
2015-10-15 19:08:05 +0000

    Don't make multiple calls to country info api #137

--- leaderboard/static/js/leaderboard.js
+++ leaderboard/static/js/leaderboard.js
@@ -1,18 +1,8 @@
 var Leaderboard = require('./react.leaderboard.js');
 
 module.exports = function (config) {
-  window.fetch(config.countriesInfoUrl).then(function (response) {
-    return response.json();
-  }).then(function (data) {
-
-    config.countries = {};
-    data.map(function(country) {
-      config.countries[country.iso2] = country;
-    });
-
-    React.render(
-      <Leaderboard config={config} />,
-      document.getElementById('leaderboard-container')
-    );
-  });
+  React.render(
+    <Leaderboard config={config} />,
+    document.getElementById('leaderboard-container')
+  );
 };
