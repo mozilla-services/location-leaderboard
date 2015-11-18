bd9d8a27916cca51babdd3d8b46223b252765296
Jared Kerim <jared.l.kerim@gmail.com>
2015-11-18 21:09:09 +0000

    Successfully passing 0 tests

---
+++ leaderboard/static/js/tests/leaderprofile.tests.js
@@ -0,0 +1,5 @@
+global.React = require("react");
+
+var expect = require("chai").expect;
+var LeaderCountryRow = require("../leaderprofile.react.js").LeaderCountryRow;
+var sd = require("skin-deep");
