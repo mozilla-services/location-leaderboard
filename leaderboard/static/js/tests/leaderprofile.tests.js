d8bf5a83720c936410ab070cb9f1f3779c0244a5
Jared Kerim <jared.l.kerim@gmail.com>
2015-11-19 16:40:45 +0000

    Next steps

--- leaderboard/static/js/tests/leaderprofile.tests.js
+++ leaderboard/static/js/tests/leaderprofile.tests.js
@@ -2,4 +2,18 @@ global.React = require("react");
 
 var expect = require("chai").expect;
 var LeaderCountryRow = require("../leaderprofile.react.js").LeaderCountryRow;
-var sd = require("skin-deep");
+var skinDeep = require("skin-deep");
+
+describe('LeaderCountryRow Tests', function () {
+  var vdom, instance;
+
+  beforeEach(function() {
+    var tree = skinDeep.shallowRender(React.createElement(LeaderCountryRow, {selection: {}, rank: {}}));
+
+    instance = tree.getMountedInstance();
+    vdom = tree.getRenderOutput();
+  });
+
+  it('should do the stuff', function () {
+  });
+});
