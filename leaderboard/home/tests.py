d2b1710f190c34e13cdc113bc9ed0dfcf93bbdc5
Jared Kerim <jared.l.kerim@gmail.com>
2016-01-15 20:50:19 +0000

    Add a __heartbeat__ endpoint #184
    - Add a view that tests if it can connect to the db
    - Add test
    - Add docs

--- leaderboard/home/tests.py
+++ leaderboard/home/tests.py
@@ -1,7 +1,8 @@
 import json
 
-from django.conf import settings
+import mock
 from django.core.urlresolvers import reverse
+from django.db import OperationalError
 from django.test import TestCase
 
 
@@ -16,10 +17,10 @@ class VersionViewTests(TestCase):
 
     def test_version_view_returns_git_settings(self):
         git_settings = {
-            'commit': 'abcdef',
-            'source': 'git/repo/path',
-            'tag': 'test-0.1',
-            'version': '0.1',
+            'GIT_COMMIT': 'abcdef',
+            'GIT_SOURCE': 'git/repo/path',
+            'GIT_TAG': 'test-0.1',
+            'GIT_VERSION': '0.1',
         }
 
         with self.settings(**git_settings):
@@ -30,8 +31,22 @@ class VersionViewTests(TestCase):
 
             version_data = json.loads(response.content)
             self.assertEqual(version_data, {
-                'commit': settings.GIT_COMMIT,
-                'source':  settings.GIT_SOURCE,
-                'tag': settings.GIT_TAG,
-                'version': settings.GIT_VERSION,
+                'commit': git_settings['GIT_COMMIT'],
+                'source': git_settings['GIT_SOURCE'],
+                'tag': git_settings['GIT_TAG'],
+                'version': git_settings['GIT_VERSION'],
             })
+
+
+class HearbeatViewTests(TestCase):
+
+    def test_heartbeat_view_returns_200_if_able_to_reach_db(self):
+        response = self.client.get(reverse('home-heartbeat'))
+        self.assertEqual(response.status_code, 200)
+
+    @mock.patch('leaderboard.home.views.connections')
+    def test_heartbeat_view_fails_if_unable_to_reach_db(self, mocked_conn):
+        mocked_conn['default'].cursor.side_effect = OperationalError
+
+        response = self.client.get(reverse('home-heartbeat'))
+        self.assertEqual(response.status_code, 400)
--- leaderboard/home/views.py
+++ leaderboard/home/views.py
@@ -1,7 +1,8 @@
 from django.conf import settings
+from django.db import connections, OperationalError
 from django.views.generic import TemplateView
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class LandingView(TemplateView):
@@ -17,3 +18,18 @@ class VersionView(APIView):
             'tag': settings.GIT_TAG,
             'version': settings.GIT_VERSION,
         }, content_type='application/json')
+
+
+class HeartbeatView(APIView):
+
+    def get(self, request):
+        response = 200
+
+        try:
+            # Test that we are able to connect to the
+            # database
+            connections['default'].cursor()
+        except OperationalError:
+            response = 400
+
+        return Response(status=response)
